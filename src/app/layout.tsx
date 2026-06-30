d6015c05c90980f35ecfa8802da0f9284283183d
Yohan Guyot <yohanguyot.me@gmail.com>
2026-06-30 20:23:06 +0200

    fix missing analytics and speed insights in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,6 +27,8 @@ export default function RootLayout({
   return (
     <html lang="en" className={`${geistSans.variable} ${geistMono.variable}`}>
       <body>{children}</body>
+      <Analytics />
+      <SpeedInsights />
     </html>
   );
 }
